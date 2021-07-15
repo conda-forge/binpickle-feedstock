4d4b2da2fbb8b2d4bfda09409a00fb036953717f
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-07-15 10:24:32 -0600

    deadline fix

--- recipe/conftest.py
+++ recipe/conftest.py
@@ -1,3 +1,4 @@
 import hypothesis
 
-hypothesis.settings.register_profile('default', deadline=500)
+hypothesis.settings.default.deadline = 500
+# hypothesis.settings.register_profile('default', deadline=500)
